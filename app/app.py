36a19da713f6bede0df8c6a0589ef7666ff71cc4
jsbailey <jason.bailey@imperial.ac.uk>
2024-01-28 22:04:45 +0000

    workflow

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home    ():
+    #return 'Hello x, World!'
+    uri='http://book-api.h8bpb6h5gtg2ecga.uksouth.azurecontainer.io/'
+    response = requests.get(uri)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse JSON data from the response
+        json_data = response.json()
+        # Render a template with the JSON data
+        return json_data
+    else:
+        # Return an error message if the request was not successful
+        return f'Error: {response.status_code} - Unable to fetch data from API'
 
 if __name__ == '__main__':
     app.run(debug=True)
